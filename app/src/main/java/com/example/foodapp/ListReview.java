ec2fb6243fafa58f4a799e77d0de42133148574b
Pasindu Shyaminda <it21016202@my.sliit.lk>
2022-09-20 18:52:04 +0530

    fine tune

---
+++ app/src/main/java/com/example/foodapp/ListReview.java
@@ -0,0 +1,20 @@
+package com.example.foodapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+public class ListReview extends AppCompatActivity {
+
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_review);
+
+    }
+}
